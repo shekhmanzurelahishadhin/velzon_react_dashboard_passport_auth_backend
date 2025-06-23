c7be113d4414a1ef68b14d06cde86a82a9b12443
shekhmanzurmit05 <shekhmanzurmit05@gmail.com>
2025-06-23 16:24:49 +0600

    Small change

---
+++ app/Http/Controllers/API/ProjectCategoryController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProjectCategory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProjectCategoryController extends Controller
+{
+    public function index()
+    {
+        return ProjectCategory::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:100',
+        ],[
+            'name.required'=>'Name is required.',
+            'name.max'=>'Name must be more than 100 character.',
+        ]);
+
+        return ProjectCategory::create([
+            'name' => $request->name,
+            'created_by' => Auth::id(),
+        ]);
+    }
+
+    public function show(ProjectCategory $projectCategory)
+    {
+        return $projectCategory;
+    }
+
+    public function update(Request $request, ProjectCategory $projectCategory)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $projectCategory->update([
+            'name' => $request->name,
+            'updated_by' => Auth::id(),
+        ]);
+
+        return $projectCategory;
+    }
+
+    public function destroy(ProjectCategory $projectCategory)
+    {
+        $projectCategory->delete();
+        return response()->json(['message' => 'Deleted successfully']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\Auth\RegisterController;
 use App\Http\Controllers\API\Auth\LoginController;
+use App\Http\Controllers\API\ProjectCategoryController;
 
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/forget-password', [LoginController::class, 'forgetPassword']);
 Route::post('/reset-password', [LoginController::class, 'resetPassword']);
+Route::middleware('auth:api')->post('/logout', [LoginController::class, 'logout']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
-Route::middleware('auth:api')->post('/logout', [LoginController::class, 'logout']);
+
+Route::middleware('auth:api')->group(function (){
+    Route::apiResource('project-categories',ProjectCategoryController::class);
+});
